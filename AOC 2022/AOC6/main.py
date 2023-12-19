b620f61f7680e77d059612f7a004c17c0b3e52a5
maureendusing <mdusing2018@gmail.com>
2023-12-19 12:59:08 -0500

    day 6 star 2

--- AOC 2022/AOC6/main.py
+++ AOC 2022/AOC6/main.py
@@ -8,15 +8,35 @@ while 1:
         break
     characters.append(char)
 
-while 1:
-    for index in range(0, len(characters)):
-        next_3 = [characters[index+1], characters[index+2], characters[index+3]]
-        set_3 = set(next_3)
-        if len(set_3) != len(next_3):
-            pass
-        else:
-            if characters[index] not in next_3:
-                print(index+4)
+def star_one(characters):
+    while 1:
+        for index in range(0, len(characters)):
+            next_3 = [characters[index+1], characters[index+2], characters[index+3]]
+            set_3 = set(next_3)
+            if len(set_3) != len(next_3):
+                pass
+            else:
+                if characters[index] not in next_3:
+                    print(index+4)
+                    break
+        break
+
+star_one(characters)
+def star_two(characters):
+    while 1:
+        for index in range(0, len(characters)):
+            next_14 = []
+            for i in range(0,13):
+                next_14.append(characters[index+i])
+            
+            set_14 = set(next_14)
+            if len(set_14) != len(next_14):
+                pass
+            else:
+                print(index+14)
                 break
-print(characters)
+        break
+
+star_two(characters)
+
     
