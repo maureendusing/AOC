dfe758471342c309520ae4fcfdf13ac79c177e64
maureendusing <mdusing2018@gmail.com>
2023-12-20 09:24:27 -0500

    changes to day 3

--- AOC 2022/AOC3/main.py
+++ AOC 2022/AOC3/main.py
@@ -13,14 +13,3 @@ def star_one(lines):
                 both = value
         total = total + (charstr.index(both) + 1)
     return total
-
-def star_two(lines):
-    for line in lines:
-        half = int(len(line.strip())/2)
-        compartment1 = line[0:half]
-        compartment2 = line[half:]
-        for value in compartment1:
-            if value in compartment2:
-                both = value
-        total = total + (charstr.index(both) + 1)
-    return total
