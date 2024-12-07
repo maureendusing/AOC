6635210cbf9d527a74741988e1780f88e4a9345d
maureendusing <mdusing2018@gmail.com>
2024-12-07 18:02:05 -0500

    some fixes still tweaking

--- AOC2024/Day4/main.py
+++ AOC2024/Day4/main.py
@@ -3,16 +3,12 @@ def check_diagonal_left_up(grid, index1, index2):
         letters = ["X", "M", "A", "S"]
         found = True
         for count in range(1,4):
-            # print(f'index1: {index1}, index2: {index2}')
-            # print(f'count: {count}, letter: {letters[count]}')
             if grid[index1-count][index2-count] == letters[count]:
-                print(f'count: {count}, letter: {letters[count]}, grid: {grid[index1-count][index2-count]}')
-                # print(grid[index1+count][index2+count])
                 continue
             else: 
                 found = False
-                break
         if found == True:
+            print("Diagonal Left Up Found")
             return 1
         else:
             return 0
@@ -21,163 +17,159 @@ def check_diagonal_left_up(grid, index1, index2):
 
 
 
-# def check_vertical_up(grid, index1, index2):
-#     try:    
-#         letters = ["X", "M", "A", "S"]
-#         found = True
-#         for count in range(1,4):
-#             if grid[index1-count][index2] == letters[count]:
-#                 continue
-#             else: 
-#                 found = False
-#         if found == True:
-#             return 1
-#         else:
-#             return 0
-#     except:
-#         return 0
+def check_vertical_up(grid, index1, index2):
+    try:    
+        letters = ["X", "M", "A", "S"]
+        found = True
+        for count in range(1,4):
+            if grid[index1-count][index2] == letters[count]:
+                continue
+            else: 
+                found = False
+        if found == True:
+            print("Vertical Up Found")
+            return 1
+        else:
+            return 0
+    except:
+        return 0
 
-# def check_diagonal_right_up(grid, index1, index2):
-#     try:    
-#         letters = ["X", "M", "A", "S"]
-#         found = True
-#         for count in range(1,4):
-#             if grid[index1-count][index2+count] == letters[count]:
-#                 continue
-#             else: 
-#                 found = False
-#         if found == True:
-#             return 1
-#         else:
-#             return 0
-#     except:
-#         return 0
+def check_diagonal_right_up(grid, index1, index2):
+    try:    
+        letters = ["X", "M", "A", "S"]
+        found = True
+        for count in range(1,4):
+            if grid[index1-count][index2+count] == letters[count]:
+                continue
+            else: 
+                found = False
+        if found == True:
+            print("Diagonal Right Up Found")
+            return 1
+        else:
+            return 0
+    except:
+        return 0
 
-# def check_horizontal_left(grid, index1, index2):
-#     try:
-#         letters = ["X", "M", "A", "S"]
-#         found = True
-#         for count in range(1,4):
-#             if grid[index1][index2-count] == letters[count]:
-#                 continue
-#             else: 
-#                 found = False
-#         if found == True:
-#             return 1
-#         else:
-#             return 0
-#     except:
-#         return 0
+def check_horizontal_left(grid, index1, index2):
+    try:
+        letters = ["X", "M", "A", "S"]
+        found = True
+        for count in range(1,4):
+            if grid[index1][index2-count] == letters[count]:
+                continue
+            else: 
+                found = False
+        if found == True:
+            print("Horizontal Left Found")
+            return 1
+        else:
+            return 0
+    except:
+        return 0
 
-# def check_horizontal_right(grid, index1, index2):
-#     try:    
-#         letters = ["X", "M", "A", "S"]
-#         found = True
-#         for count in range(1,4):
-#             if grid[index1][index2+count] == letters[count]:
-#                 continue
-#             else: 
-#                 found = False
-#         if found == True:
-#             return 1
-#         else:
-#             return 0
-#     except:
-#         return 0
+def check_horizontal_right(grid, index1, index2):
+    try:    
+        letters = ["X", "M", "A", "S"]
+        found = True
+        for count in range(1,4):
+            if grid[index1][index2+count] == letters[count]:
+                continue
+            else: 
+                found = False
+        if found == True:
+            print("Horizontal Right Found")
+            return 1
+        else:
+            return 0
+    except:
+        return 0
 
-# def check_diagonal_down_left(grid, index1, index2):
-#     try:    
-#         letters = ["X", "M", "A", "S"]
-#         found = True
-#         for count in range(1,4):
-#             if grid[index1+count][index2-count] == letters[count]:
-#                 continue
-#             else: 
-#                 found = False
-#         if found == True:
-#             return 1
-#         else:
-#             return 0
-#     except:
-#         return 0
+def check_diagonal_down_left(grid, index1, index2):
+    try:    
+        letters = ["X", "M", "A", "S"]
+        found = True
+        for count in range(1,4):
+            if grid[index1+count][index2-count] == letters[count]:
+                continue
+            else: 
+                found = False
+        if found == True:
+            print("Diagonal Left Down Found")
+            return 1
+        else:
+            return 0
+    except:
+        return 0
 
-# def check_vertical_down(grid, index1, index2):
-#     try:    
-#         letters = ["X", "M", "A", "S"]
-#         found = True
-#         for count in range(1,4):
-#             if grid[index1+count][index2] == letters[count]:
-#                 # print(grid[index1+count][index2+count])
-#                 continue
-#             else: 
-#                 found = False
-#         if found == True:
-#             return 1
-#         else:
-#             return 0
-#     except:
-#         return 0
+def check_vertical_down(grid, index1, index2):
+    try:    
+        letters = ["X", "M", "A", "S"]
+        found = True
+        for count in range(1,4):
+            if grid[index1+count][index2] == letters[count]:
+                # print(grid[index1+count][index2+count])
+                continue
+            else: 
+                found = False
+        if found == True:
+            print("Vertical Down Found")
+            return 1
+        else:
+            return 0
+    except:
+        return 0
     
-# def check_diagonal_down_right(grid, index1, index2):
-#     # print(f'index1: {index1}, index2: {index2}')
-#     try:    
-#         letters = ["X", "M", "A", "S"]
-#         found = True
-#         for count in range(1,4):
-#             if grid[index1+count][index2+count] == letters[count]:
-#                 # print(grid[index1+count][index2+count])
-#                 continue
-#             else: 
-#                 found = False
-#         if found == True:
-#             return 1
-#         else:
-#             return 0
-#     except:
-#         return 0
+def check_diagonal_down_right(grid, index1, index2):
+    # print(f'index1: {index1}, index2: {index2}')
+    try:    
+        letters = ["X", "M", "A", "S"]
+        found = True
+        for count in range(1,4):
+            if grid[index1+count][index2+count] == letters[count]:
+                # print(grid[index1+count][index2+count])
+                continue
+            else: 
+                found = False
+        if found == True:
+            print("Diagonal Right Down Found")
+            return 1
+        else:
+            return 0
+    except:
+        return 0
 
 def find_neighbor(grid, index1, index2):
-    # print(f'grid: {grid}')
-    # print(f'index1: {index1}')
-    # print(f'index2: {index2}')
+    ##needs to be 4 - not currently finding 5
+    #print(f'diagonal_left_up: {check_diagonal_left_up(grid, index1, index2)}')
 
-    # diagonal_left_up=grid[index1-1][index2-1]
-    # vertical_up = grid[index1-1][index2]
-    # diagonal_right_up = grid[index1-1][index2+1]
-    # horizontal_left = grid[index1][index2-1]
-    # horizontal_right = grid[index1][index2+1]
-    # diagonal_down = grid[index1+1][index2-1]
-    # vertical_down = grid[index1+1][index2]
-    # diagonal_down_right = grid[index1+1][index2+1]
-    test_case = check_diagonal_left_up(grid, 5, 6)
-    print(f'test_case: {test_case}')
-    ##needs to be 4 - not
-    print(f'diagonal_left_up: {check_diagonal_left_up(grid, index1, index2)}')
-
-    ##needs to be 2 
+    ##needs to be 2 - good
     # print(f'vertical_up: {check_vertical_up(grid, index1, index2)}')
 
-    ##needs to be 4 - not
+    ##needs to be 4 - good
     # print(f'check_diagonal_right_up: {check_vertical_up(grid, index1, index2)}')
 
-    ##needs to be 2 - not 
+    ##needs to be 2 - good
     # print(f'check_horizontal_left: {check_horizontal_left(grid, index1, index2)}')
 
-    ##needs to be 3
+    ##needs to be 3 - good
     # print(f'check_horizontal_right: {check_horizontal_right(grid, index1, index2)}')
 
-    ##needs to be 1
+    ##needs to be 1 - good
     # print(f'check_diagonal_down_left: {check_diagonal_down_left(grid, index1, index2)}')
 
-    ##needs to be 1
+    ##needs to be 1 - good
     # print(f'check_vertical_down: {check_vertical_down(grid, index1, index2)}')
 
-    ##needs to be 1 
+    ##needs to be 1 - good
     ## why is it saying that index 2,2 is right? 
     # print(f'check_diagonal_down_right: {check_diagonal_down_right(grid, index1, index2)}')
 
-    sum = check_diagonal_left_up(grid, index1, index2)
-    # sum = check_diagonal_left_up(grid, index1, index2)+check_vertical_up(grid, index1, index2)+check_diagonal_right_up(grid, index1, index2)+check_horizontal_left(grid, index1, index2)+check_horizontal_right(grid, index1, index2)+check_diagonal_down_left(grid, index1, index2)+check_vertical_down(grid, index1, index2)+check_diagonal_down_right(grid, index1, index2)
+    ##somehow is getting 9,1 as an option
+    if check_diagonal_left_up(grid, index1, index2) == 1:
+        print(f'index1: {index1}, index2: {index2}')
+
+    sum = check_diagonal_left_up(grid, index1, index2)+check_vertical_up(grid, index1, index2)+check_diagonal_right_up(grid, index1, index2)+check_horizontal_left(grid, index1, index2)+check_horizontal_right(grid, index1, index2)+check_diagonal_down_left(grid, index1, index2)+check_vertical_down(grid, index1, index2)+check_diagonal_down_right(grid, index1, index2)
     return sum
 
 
@@ -193,9 +185,8 @@ def part_one(input):
         letters = list(line)
         # print(f'letters: {letters}')
         grid.append(letters)
-    
-    for index_row in range(0, len(letters)-1):
-        for index_col in range(0, len(data)-1):
+    for index_row in range(0, len(letters)):
+        for index_col in range(0, len(data)):
             # print(grid[index_row][index_col])
             if grid[index_row][index_col] == "X":
                 # print("X")
