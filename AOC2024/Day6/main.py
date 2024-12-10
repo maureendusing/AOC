e59e8f7908d6dd1320f9e229c0ec54353e0abbbc
maureendusing <mdusing2018@gmail.com>
2024-12-09 21:42:49 -0500

    infinite loops

--- AOC2024/Day6/main.py
+++ AOC2024/Day6/main.py
@@ -12,14 +12,52 @@
 
 
 def move_upward(grid, index1, index2):
+    while index1-1 > 0 and grid[index1-1][index2] != "#":
+        grid[index1-1][index2] = "X"
+        index1=index1-1
+        move_upward(grid, index1, index2)
+
     if index1-1 < 0: 
         return "stop"
-    else:
-        if grid[index1-1][index2] != "#":
-            grid[index1-1][index2] = "X"
-        else: 
+    if grid[index1-1][index2] == "#":
+        move_right(grid, index1,index2)
+        print(f'HIT SOMETHING, grid: {grid}')
+        
+def move_right(grid, index1,index2):
+    try: 
+        while grid[index1][index2+1] != "#":
+            grid[index1][index2+1] = "X"
+            index2=index2+1
             move_right(grid, index1, index2)
+        if grid[index1][index2+1] == "#":
+            move_down(grid, index1,index2)
+            print(f'HIT SOMETHING, grid: {grid}')
+    except: 
+        return "stop"
     
+def move_down(grid, index1, index2):
+    try:
+        while grid[index1+1][index2] != "#":
+            grid[index1+1][index2] = "X"
+            index1=index1+1
+            move_down(grid, index1, index2)
+        if grid[index1+1][index2] == "#":
+            move_down(grid, index1,index2)
+            print(f'HIT SOMETHING, grid: {grid}')
+    except: 
+        return "stop"
+    
+def move_left(grid, index1, index2):
+    while index2-1 > 0 and grid[index1][index2-1] != "#":
+        grid[index1][index2-1] = "X"
+        index2=index2-1
+        move_upward(grid, index1, index2)
+
+    if index1-1 < 0: 
+        return "stop"
+    if grid[index1-1][index2] == "#":
+        move_upward(grid, index1,index2)
+        print(f'HIT SOMETHING, grid: {grid}')
 
 def part_one(input):
     file = open(input, 'r')
@@ -33,7 +71,11 @@ def part_one(input):
     for index_row in range(0, len(letters)):
         for index_col in range(0, len(data)):
             if grid[index_row][index_col] == "^":
+                starting_row_index = index_row
+                starting_col_index = index_col
                 print(f'starting point: {index_row}, {index_col}') 
+    
+    move_upward(grid, starting_row_index, starting_col_index)
 
     print(grid)
 
