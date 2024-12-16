261af49a7068e0f4dbe5e2ac85136f58ee386759
maureendusing <mdusing2018@gmail.com>
2024-12-16 10:10:38 -0500

    not sure what I am doing

--- AOC2024/Day13/main.py
+++ AOC2024/Day13/main.py
@@ -1,22 +1,22 @@
 def process_systems_of_equations(button_a, button_b, prize):
     """doing this by solving for b"""
 
-    subtraction_variable = button_a[1]*button_b[0] - button_b[1]*button_a[0]
-    ### this is wrong 
-    subtract_equals_to = (button_a[1])*prize[0] - (prize[1])*button_a[0]
-    
-    print(f'subtraction_variable: {subtraction_variable}, subtraction_equals:{subtract_equals_to}')
+    determinant=button_a[0]*button_b[1]-button_a[1]*button_b[0]
+    if determinant !=0:
 
-    b_button_pushes = subtract_equals_to/subtraction_variable
+        subtraction_variable = button_a[1]*button_b[0] - button_b[1]*button_a[0]
+        subtract_equals_to = (button_a[1])*prize[0] - (prize[1])*button_a[0]
+
+        b_button_pushes = subtract_equals_to/subtraction_variable
 
 
-    a_button_pushes = (prize[0] - button_b[0]*b_button_pushes)/button_a[0]
-
-    print(f'a_button_pushes: {a_button_pushes},b_button_pushes: {b_button_pushes}')
+        a_button_pushes = (prize[0] - button_b[0]*b_button_pushes)/button_a[0]
+    if determinant == 0:
+        print(f'button_a: {button_a}, button_b: {button_b}, prize: {prize}')
     return a_button_pushes, b_button_pushes
 
 def calculate_total(a_button_pushes, b_button_pushes, prize):
-    if a_button_pushes%1 ==0 and b_button_pushes%1==0 and a_button_pushes < 100 and b_button_pushes < 100:
+    if a_button_pushes%1 ==0 and b_button_pushes%1==0 and a_button_pushes <= 100 and b_button_pushes <= 100 and a_button_pushes > 0 and b_button_pushes > 0:
         total = 3*a_button_pushes + b_button_pushes
     else: 
         return 0
@@ -34,20 +34,17 @@ def part_one(input):
     count = 0 
     for line in lines:
         count = count + 1
-        print(f'count: {count}')
-        print(f'line: {line}')
         if count%4 !=0:
             button, formula = line.strip().split(':')
             x, y = formula.strip().split(', ')
 
             if button == "Button A":
-                _, x = x.strip().split('+')
-                _, y = y.strip().split('+')
-                print(f'x: {x}, y:{y}')
+                _, x = x.strip().split('+') or 1
+                _, y = y.strip().split('+') or 1
                 button_a = [int(x),int(y)]
             if button == "Button B":
-                _, x = x.strip().split('+')
-                _, y = y.strip().split('+')
+                _, x = x.strip().split('+') or 1
+                _, y = y.strip().split('+') or 1
                 button_b = [int(x),int(y)]
             if button == "Prize":
                 _, x = x.strip().split('=')
@@ -56,6 +53,8 @@ def part_one(input):
 
         else: 
             button_a_pushes, button_b_pushes = process_systems_of_equations(button_a, button_b, prize)
+            # if calculate_total(button_a_pushes, button_b_pushes, prize) == 0:
+            #     print(f'didnt work {button_a}, {button_b}, {prize}')
             tokens = tokens + calculate_total(button_a_pushes, button_b_pushes, prize)
             
             button_a = []
@@ -63,4 +62,4 @@ def part_one(input):
             prize = []
     print(f'tokens:{tokens}')
 
-part_one('input.txt')
+part_one('test-input.txt')
