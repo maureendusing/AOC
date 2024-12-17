2a4ababa4ef7ca11a40ae3ca05e2eb43407791fb
maureendusing <mdusing2018@gmail.com>
2024-12-17 09:36:14 -0500

    day5 testing

--- AOC2024/Day5/main.py
+++ AOC2024/Day5/main.py
@@ -1,10 +1,26 @@
+def order_pages(pages, current_order):
+    if pages[0] in current_order:
+        index_first_page=current_order.index(pages[0])
+        print(index_first_page)
+    else:
+        current_order.append(pages[0])
+        current_order.append(pages[1])
+        print(current_order)
+
 def part_one(input):
     file = open(input, 'r')
     lines = file.readlines()
-
+    current_order = []
     for line in lines:
         pages = line.strip().split('|')
+        if pages[0] == '':
+            print('start processing repports')
+        else:
+            order_pages(pages, current_order)
+        
+
         print(f'pages: {pages}')
+        
 
 
 part_one('test-input.txt')
